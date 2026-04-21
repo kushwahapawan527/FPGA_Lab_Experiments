6b23762accd5882b74340f12b824951fed4c3a27
Pawan Kushwah <kushwahapawan527@gmail.com>
2026-04-21 15:33:58 +0530

    Add 1-to-4 demultiplexer module

---
+++ 03_Multiplexer_Demultiplexer/demux_1to4.v
@@ -0,0 +1,17 @@
+module demux_1to4(
+    input D,
+    input [1:0] S,
+    output reg [3:0] Y
+);
+
+always @(*) begin
+    Y = 4'b0000;
+    case(S)
+        2'b00: Y[0] = D;
+        2'b01: Y[1] = D;
+        2'b10: Y[2] = D;
+        2'b11: Y[3] = D;
+    endcase
+end
+
+endmodule
