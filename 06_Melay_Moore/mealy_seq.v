6f757f5d40bc14ff79f1e0d084fe3b9bb1ca5a59
Pawan Kushwah <kushwahapawan527@gmail.com>
2026-04-23 18:32:02 +0530

    Implement Mealy state machine for sequence detection

---
+++ 06_Melay_Moore/mealy_seq.v
@@ -0,0 +1,49 @@
+module mealy_seq (
+    input clk,
+    input rst,
+    input x,
+    output reg y
+);
+
+reg [1:0] state, next;
+
+parameter S0=2'b00, S1=2'b01, S2=2'b10, S3=2'b11;
+
+// State update
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        state <= S0;
+    else
+        state <= next;
+end
+
+// Next state + output
+always @(*) begin
+    y = 0;
+    case (state)
+        S0: begin
+            if (x) next = S1;
+            else   next = S0;
+        end
+
+        S1: begin
+            if (x) next = S1;
+            else   next = S2;
+        end
+
+        S2: begin
+            if (x) next = S3;
+            else   next = S0;
+        end
+
+        S3: begin
+            if (x) begin
+                next = S1;
+                y = 1;   // detect 1011
+            end
+            else next = S2;
+        end
+    endcase
+end
+
+endmodule
